c39723883d15cab34a713906f389a07b64805bf7
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-04-24 14:24:02 -0400

    updated

--- Terraform/ecr.tf
+++ Terraform/ecr.tf
@@ -1,10 +1,11 @@
 module "ecr" {
    count = var.create_ecr_repos ? 1: 0
-   source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/ecr"
+   source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/ecr?ref=v1.2"
    stack_name = var.stack_name 
    ecr_repo_names = var.ecr_repo_names
    tags = var.tags
    create_env_specific_repo = var.create_env_specific_repo
+   resource_prefix = var.stack_name
    env = terraform.workspace
    enable_ecr_replication = var.enable_ecr_replication
    replication_destination_registry_id = var.replication_destination_registry_id
