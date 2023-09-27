e66f7a668f7322e939e3af38c3ed74809d3a9156
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-09-27 13:27:19 -0400

    updated

--- Terraform/monitoring.tf
+++ Terraform/monitoring.tf
@@ -31,7 +31,7 @@ module "monitoring" {
   sumologic_access_id  = var.sumologic_access_id
   sumologic_access_key = var.sumologic_access_key
   microservices        = var.microservices
-  new_relic_account_id      = var.newrelic_account_id
+  newrelic_account_id      = var.newrelic_account_id
   newrelic_api_key        = var.newrelic_api_key
   service              = var.microservices
   program              = var.program
