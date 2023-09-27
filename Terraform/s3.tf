39e9da32d197fb8e65803776503908b66d066887
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-09-27 14:31:43 -0400

    updated

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -14,7 +14,7 @@ module "s3" {
 
 module "s3-replication-source" {
   count = var.create_s3_replication ? 1 : 0
-  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/s3-replication-source?ref=v1.9"
+  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/s3-replication-source?ref=gmb"
   destination_bucket_name = var.destination_bucket_name 
   env =  terraform.workspace
   source_bucket_name = var.source_bucket_name
@@ -27,7 +27,7 @@ module "s3-replication-source" {
 
 module "s3-replication-destination" {
   count = var.enable_s3_replication ? 1 : 0
-  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/s3-replication-destination?ref=v1.9"
+  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/s3-replication-destination?ref=gmb"
   destination_bucket_name = var.destination_bucket_name 
   tags = var.tags
   replication_role_arn = var.replication_role_arn
