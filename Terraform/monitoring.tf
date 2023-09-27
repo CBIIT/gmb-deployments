e94d0b0b21205ddb575129da3cdd4cca3661b758
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-09-27 12:57:12 -0400

    updated

--- Terraform/monitoring.tf
+++ Terraform/monitoring.tf
@@ -28,7 +28,7 @@ module "monitoring" {
   resource_prefix     = "${var.project}-${terraform.workspace}"
   app                  = var.stack_name
   tags                 = var.tags
-  #sumologic_access_id  = var.sumologic_access_id
-  #sumologic_access_key = var.sumologic_access_key
+  sumologic_access_id  = var.sumologic_access_id
+  sumologic_access_key = var.sumologic_access_key
   microservices        = var.microservices
 }
