116baa901b50a5076a397d6b4b8e4b8d79a79948
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2024-04-11 16:23:56 -0400

    added Neo4j Dump and Restore jenkinsfiles and their corresponding playbooks

---
+++ Jenkins/DataRestore.groovy
@@ -0,0 +1,94 @@
+
+@Library('datacommons-jenkins-shared-library@v1.1') _
+
+def getLabelForEnvironment(environment) {
+	if (environment == "stage" || environment == "prod"){
+		return ""
+	}else {
+		return "slave-ncias-q2944-c"
+	}
+}
+
+
+pipeline {
+	agent {
+		node {
+			label getLabelForEnvironment(params.Environment)
+		}
+	}
+
+	parameters {
+    extendedChoice( 
+        name: 'Environment', 
+        defaultValue: 'dev', 
+        description: 'Choose the environment to build', 
+        type: 'PT_SINGLE_SELECT',
+        value: 'dev,qa,stage,prod' )
+    string(defaultValue: "", 
+        description: 'Name of the dump file to use', 
+        name: 'DumpFileName')
+    
+    }
+
+  tools {
+  	maven 'Default' 
+    jdk 'Default' 
+  }
+ environment {
+    DUMP_FILE = "${params.DumpFileName}"
+	  TIER      = "${params.Environment}"
+    SLACK_SECRET  = "gmb_slack_url"
+    PROJECT       = "gmb"
+    S3_BUCKET     = "crdc-gmb-dev-neo4j-data-backup"
+ }
+  stages{
+
+  	stage('create inventory'){
+ 		steps {
+ 		  wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+			    ansiblePlaybook( 
+                playbook: '${WORKSPACE}/ansible/hostfile.yml',
+                inventory: '${WORKSPACE}/ansible/hosts',
+                extraVars: [
+                  tier: "${params.Environment}",
+						      project_name: "${PROJECT}",
+                  workspace: "$WORKSPACE"
+						    ],
+                colorized: true)
+		  }
+ 		}
+    
+  }
+  stage("download dump"){
+    steps{
+      wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+			    ansiblePlaybook( 
+                playbook: '${WORKSPACE}/ansible/download-dump.yml',
+                inventory: '${WORKSPACE}/ansible/hosts',
+                colorized: true)
+		  }
+    }
+  }
+	stage('restore data dump'){
+		steps{			
+			ansiblePlaybook( 
+                playbook: '${WORKSPACE}/ansible/dump-restore.yml',
+                inventory: '${WORKSPACE}/inventory/hosts',
+                colorized: true)
+
+		}
+	}
+	
+  }
+  post {
+    always {
+       notify(
+            secretPath: "notification/slack",
+            secretName: "${env.SLACK_SECRET}"
+        ) 
+      }
+    cleanup {
+      cleanWs()
+      }
+  }
+}
